d4fde2ca02ef27157f14d8d64a14af0cab19b9e6
SirBenedick <beneweb@outlook.de>
2019-01-27 17:58:43 +0100

    Fixed String conversion to float numbers. String(3,4)  Will now be properly converted to Float(3.4)

--- MA3/lineareRegression.rb
+++ MA3/lineareRegression.rb
@@ -35,12 +35,12 @@ print "Zahlen eingeben X: "
 varX = gets.chomp.split
 #varX = "56 70 62 71 81 58 60 79 73 61".split
 varFloatX =[] 
-varX.each {|x| varFloatX << x.to_f}
+varX.each {|x| varFloatX << x.gsub(",",".").to_f}
 print "Zahlen eingeben Y: "
 varY = gets.chomp.split
 #varY = "165 175 167 175 180 166 169 180 176 168".split
 varFloatY =[] 
-varY.each {|x| varFloatY << x.to_f}
+varY.each {|x| varFloatY << x.gsub(",",".").to_f}
 
 puts "Empirischer Miteelwert X: #{empirischerMittelwert(varFloatX)}"
 puts "empirischeVarianz X: #{empirischeVarianz(varFloatX)}"
